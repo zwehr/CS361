df37d91a07deddefa5285420a64b860a90e449ca
zwehr <zwehrmeister@gmail.com>
2023-01-30 01:04:58 -0500

    make markers clickable

--- skate-spots/src/components/SpotsMap.js
+++ skate-spots/src/components/SpotsMap.js
@@ -2,10 +2,13 @@ import { useState, useEffect } from 'react'
 import Map, { Marker, Popup } from 'react-map-gl'
 import { collection, getDocs } from 'firebase/firestore'
 import { db } from '../firebase/config'
+import MapLegend from './MapLegend'
+import '../styles/SpotsMap.css'
 import pin from '../images/location-pin.png'
 
 export default function SpotsMap() {
   const [spots, setSpots] = useState([])
+  const [popupInfo, setPopupInfo] = useState('first-text')
   const [selectedSpot, setSelectedSpot] = useState(null)
   const [showPopup, setShowPopup] = useState(false)
 
@@ -23,23 +26,36 @@ export default function SpotsMap() {
   }
 
   return (
-    <Map
-      initialViewState={{
-        longitude: -20,
-        latitude: 20,
-        zoom: 2.5
-      }}
-      style={{ width: "100vw", height: "100vh" }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      mapboxAccessToken={process.env.REACT_APP_MB}
-    >
+    <div className='SpotsMap'>
+      <Map
+        initialViewState={{
+          longitude: -20,
+          latitude: 20,
+          zoom: 2.5
+        }}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={process.env.REACT_APP_MB}
+      >
 
-      {spots.map((spot) => (
-        <Marker latitude={spot.lat} longitude={spot.lng}>
-          <img src={pin} />
-        </Marker>
-      ))}
+        {spots.map((spot) => (
+          <Marker
+            className='marker'
+            latitude={spot.lat}
+            longitude={spot.lng}
+            onClick={e => {
+              e.originalEvent.stopPropagation();
+              setPopupInfo(spot.name)
+            }}
+          >
+            <img src={pin} />
+          </Marker>
+        ))}
 
-    </Map>
+      </Map>
+      <div className='map-legend-container'>
+        <MapLegend />
+      </div>
+    </div>
   );
 }
