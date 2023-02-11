18cb2e40fe5527e6adfa92f65eed8a64549dcdbd
zwehr <zwehrmeister@gmail.com>
2023-02-10 19:13:51 -0500

    add flexbox styling

--- skate-spots/src/components/AddSpot.js
+++ skate-spots/src/components/AddSpot.js
@@ -78,61 +78,68 @@ export default function AddSpot() {
   return (
     <div className='AddSpot'>
       <h2>Add Spot</h2>
-      <div className='add-map-container'>
-        <Map
-          initialViewState={{
-            longitude: -30,
-            latitude: 30,
-            zoom: 2
-          }}
-          style={{ width: "70vw", height: "60vh", margin: "0 auto", borderRadius: "10px", cursor: "crosshair" }}
-          mapStyle="mapbox://styles/mapbox/streets-v9"
-          mapboxAccessToken={process.env.REACT_APP_MB}
-          onClick={handleClick}
-          className="add-map"
-        >
-
-        </Map>
+      <div className='map-form-container'>
+        <div className='add-map-container half-map-form-container'>
+          <Map
+            initialViewState={{
+              longitude: -30,
+              latitude: 30,
+              zoom: 1
+            }}
+            style={{ width: '100%', height: '560px', cursor: 'crosshair', borderRadius: '10px' }}
+            mapStyle='mapbox://styles/mapbox/streets-v9'
+            mapboxAccessToken={process.env.REACT_APP_MB}
+            onClick={handleClick}
+            className="add-map"
+          >
+          </Map>
+        </div>
+        <div className='form-container half-map-form-container'>
+          <p className='add-instructions'><HiCursorClick /> <strong>Click on the map to fill latitude and longitude fields.</strong></p>
+          <form onSubmit={handleSubmit} className="add-form">
+            <label htmlFor="lat">Latitude:
+              <input type="text" name="lat" id="lat" value={lat} onChange={(e) => setLat(e.target.value)} required />
+            </label>
+            <label htmlFor="lng">Longitude:
+              <input type="text" name="lng" id="lng" value={lng} onChange={(e) => setLng(e.target.value)} required />
+            </label>
+            <label htmlFor="name">Name:
+              <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+            </label>
+            <label htmlFor='type'>Type:
+              <select value={type} onChange={handleSelectChange}>
+                <option value='handrail'>Handrail</option>
+                <option value='stairs'>Stairs</option>
+                <option value='gap'>Gap</option>
+                <option value='ledge'>Ledge</option>
+                <option value='pad'>Manual Pad</option>
+                <option value='hubba'>Hubba</option>
+                <option value='curb'>Curb</option>
+                <option value='bump'>Bump to bar</option>
+                <option value='misc'>Misc</option>
+              </select>
+            </label>
+            <label htmlFor='skate-stopped'>Skate-stopped?
+              <input type='radio' value='yes' name='skate-stopped' checked={skateStopped === 'yes'} onChange={handleRadioChange} />Yes
+              <input type='radio' value='no' name='skate-stopped' checked={skateStopped === 'no'} onChange={handleRadioChange} />No
+            </label><br></br>
+            <label htmlFor='tag'>Tags:
+              <div className='tags-container'>{tags && <TagBubblesInteractive tags={tags} handleDeleteClick={handleDeleteClick} />}</div>
+              <input type='text' name='tag' id='tag' value={tag} onChange={handleTagChange} />
+              <p className='tag-directions'>(Type a tag and press SPACE to add it to the list. Use hyphens instead of spaces in tags, e.g. ride-on-grind)</p>
+            </label>
+            <label htmlFor='description'>Description:
+              <textarea
+                name='description'
+                id='description'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required></textarea>
+            </label>
+            <input type='submit' value='Submit' />
+          </form>
+        </div>
       </div>
-      <p><HiCursorClick /> <strong>Click on the map to fill latitude and longitude fields.</strong></p>
-      <div className='form-container'>
-        <form onSubmit={handleSubmit} className="add-form">
-          <label htmlFor="lat">Latitude:
-            <input type="text" name="lat" id="lat" value={lat} onChange={(e) => setLat(e.target.value)} required />
-          </label>
-          <label htmlFor="lng">Longitude:
-            <input type="text" name="lng" id="lng" value={lng} onChange={(e) => setLng(e.target.value)} required />
-          </label>
-          <label htmlFor="name">Name:
-            <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
-          </label>
-          <label htmlFor='type'>Type:
-            <select value={type} onChange={handleSelectChange}>
-              <option value='handrail'>Handrail</option>
-              <option value='stairs'>Stairs</option>
-              <option value='gap'>Gap</option>
-              <option value='ledge'>Ledge</option>
-              <option value='pad'>Manual Pad</option>
-              <option value='hubba'>Hubba</option>
-              <option value='curb'>Curb</option>
-              <option value='bump'>Bump to bar</option>
-              <option value='misc'>Misc</option>
-            </select>
-          </label>
-          <label htmlFor='skate-stopped'>Skate-stopped?
-            <input type='radio' value='yes' name='skate-stopped' checked={skateStopped === 'yes'} onChange={handleRadioChange} />Yes
-            <input type='radio' value='no' name='skate-stopped' checked={skateStopped === 'no'} onChange={handleRadioChange} />No
-          </label><br></br>
-          <label htmlFor='tag'>Tags: {tags && <TagBubblesInteractive tags={tags} handleDeleteClick={handleDeleteClick} />}
-            <input type='text' name='tag' id='tag' value={tag} onChange={handleTagChange} />
-            <p className='tag-directions'>(Type a tag and press SPACE to add it to the list. Use hyphens instead of spaces in tags, e.g. ride-on-grind)</p>
-          </label>
-          <label htmlFor='description'>Description:
-            <textarea name='description' id='description' value={description} onChange={(e) => setDescription(e.target.value)} required></textarea>
-          </label>
-          <input type='submit' value='Submit' />
-        </form>
-      </div>
-    </div>
+    </div >
   )
 }
