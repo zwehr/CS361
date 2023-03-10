e64c55481f12c48b73dcdc32c8ceb3b104cca231
zwehr <zwehrmeister@gmail.com>
2023-03-10 03:06:07 -0500

    add more protected routes, hide them from public nav

--- skate-spots/src/App.js
+++ skate-spots/src/App.js
@@ -1,33 +1,63 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AddSpot from './components/AddSpot'
-import EditSpot from './components/EditSpot'
-import Nav from './components/Nav'
-import SpotsMap from './components/SpotsMap'
-import Login from './components/Login'
-import SignUp from './components/SignUp'
-import Account from './components/Account'
-import SpotsList from './components/SpotsList'
-import './App.css'
-import { AuthContextProvider } from './context/AuthContext'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+import AddSpot from "./components/AddSpot";
+import EditSpot from "./components/EditSpot";
+import Nav from "./components/Nav";
+import SpotsMap from "./components/SpotsMap";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Account from "./components/Account";
+import SpotsList from "./components/SpotsList";
+import ProtectedRoute from "./components/ProtectedRoute";
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <BrowserRouter>
-        <Nav />
         <AuthContextProvider>
+          <Nav />
           <Routes>
-            <Route path='/' element={<SpotsMap />} />
-            <Route path='/add-spot' element={<AddSpot />} />
-            <Route path='/edit-spot/:id' element={<EditSpot />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/sign-up' element={<SignUp />} />
-            <Route path='/account' element={<Account />} />
-            <Route path='/spots-list' element={<SpotsList />} />
+            <Route path="/" element={<SpotsMap />} />
+            <Route
+              path="/add-spot"
+              element={
+                <ProtectedRoute>
+                  <AddSpot />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/edit-spot/:id"
+              element={
+                <ProtectedRoute>
+                  <EditSpot />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            {/* Currently not allowing sign-ups */}
+            {/* <Route path="/sign-up" element={<SignUp />} /> */}
+            <Route
+              path="/account"
+              element={
+                <ProtectedRoute>
+                  <Account />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/spots-list"
+              element={
+                <ProtectedRoute>
+                  <SpotsList />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
         </AuthContextProvider>
       </BrowserRouter>
-    </div >
+    </div>
   );
 }
 
--- skate-spots/src/components/Nav.js
+++ skate-spots/src/components/Nav.js
@@ -1,15 +1,18 @@
-import { NavLink } from 'react-router-dom'
-import '../styles/Nav.css'
+import { NavLink } from "react-router-dom";
+import "../styles/Nav.css";
+import { UserAuth } from "../context/AuthContext";
 
 export default function Nav() {
+  const { user } = UserAuth();
+
   return (
-    <nav className='Nav'>
-      <h1 className='app-name'>Skate Spots App</h1>
-      <NavLink to='/'>Home (Map)</NavLink>
-      <NavLink to='/spots-list'>Spots List</NavLink>
-      <NavLink to='/add-spot'>Add Spot</NavLink>
-      <NavLink to='/login'>Login</NavLink>
-      <NavLink to='/sign-up'>Sign Up</NavLink>
+    <nav className="Nav">
+      <h1 className="app-name">Skate Spots App</h1>
+      <NavLink to="/">Home (Map)</NavLink>
+      {user && <NavLink to="/spots-list">Spots List (Admin)</NavLink>}
+      {user && <NavLink to="/add-spot">Add Spot</NavLink>}
+      {user && <NavLink to="/account">Account</NavLink>}
+      <NavLink to="/login">Admin Login</NavLink>
     </nav>
-  )
+  );
 }
