92e0b967b39a23924cc2666a00b99c1f18cb9683
zwehr <zwehrmeister@gmail.com>
2023-03-12 00:46:14 -0500

    add search (by description) to spots list (public view)

--- skate-spots/src/components/SpotsListPublic.js
+++ skate-spots/src/components/SpotsListPublic.js
@@ -6,19 +6,44 @@ import "../styles/SpotsListPublic.css";
 
 export default function SpotListPublic() {
   const [spots, setSpots] = useState([]);
+  const [list, setList] = useState([]);
+  const [query, setQuery] = useState("");
   const spotCollectionRef = collection(db, "spots");
 
   useEffect(() => {
     const getSpots = async () => {
       const data = await getDocs(spotCollectionRef);
       setSpots(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getSpots();
   }, []);
 
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+    const results = spots.filter((spot) => {
+      if (e.target.value === "") return spots;
+      return spot.description
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase());
+    });
+    console.log(results);
+    setList(results);
+  };
+
   return (
     <div className="SpotsListPublic">
       <h2>Spots List</h2>
+      <div className="search-container">
+        Search:
+        <input
+          type="text"
+          className="tag-search"
+          placeholder="Search by description..."
+          value={query}
+          onChange={handleChange}
+        ></input>
+      </div>
       <div className="table-container">
         <table>
           <thead>
@@ -31,7 +56,7 @@ export default function SpotListPublic() {
             </tr>
           </thead>
           <tbody>
-            {spots.map((spot) => (
+            {list.map((spot) => (
               <tr key={spot.id}>
                 <td>{spot.name}</td>
                 <td>{spot.description}</td>
